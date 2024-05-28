52167ba67a69207224aa647a1a33c9894aeb7aa9
unknown <aadityastha123456@gmail.com>
2024-05-28 06:32:06 +0545

    about us started

--- script.js
+++ script.js
@@ -5,24 +5,25 @@ const emailAddress = document.querySelector(".email_")
 const submitButton = document.querySelector(".btn_")
 // const emailValue = emailAddress.value;
 
+
 menuButton.addEventListener("click", () =>{
         console.log("hello world")
         navLinks.classList.toggle("hide")
 })
 
+
 currentForm.addEventListener("submit", (e) => {
+        // const arr = []
         e.preventDefault();
         // console.log("SUBMITTED SUCCESSFULLY");
         alert("Submitted Successfully")
         // console.log(emailValue)
         console.log(emailAddress.value)
-        emailAddress.value = " "
-        // addData();
+        // emailAddress.value = " "
+        const storage ={
+                email: emailAddress.value,
+        }
+        JSON.stringify(storage)
+        localStorage.setItem("email", storage.email)
 })
 
-
-function addData(){
-        let emailValue = emailAddress.value
-        JSON.stringify(emailValue)
-        localStorage.setItem("email", emailValue)
-}
