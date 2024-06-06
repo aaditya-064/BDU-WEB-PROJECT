dccda69a9fb4b9d888927e1a10c6ddc8a883008e
unknown <aadityastha123456@gmail.com>
2024-06-06 16:10:34 +0545

    edited in responsive

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
-const menuButton = document.querySelector('.menu');
-const navLinks = document.querySelector('.nav_links')
-const currentForm = document.getElementById("main_form")
-const emailAddress = document.querySelector(".email_")
-const submitButton = document.querySelector(".btn_")
-const navBar = document.querySelector('#sticky_nav');
-const topNavDiv = document.querySelector(".top_nav")
-const namePrintArea = document.querySelector("#namePrint")
-const logOutButton = document.querySelector("#logOut")
+const menuButton = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav_links");
+const currentForm = document.getElementById("main_form");
+const emailAddress = document.querySelector(".email_");
+const submitButton = document.querySelector(".btn_");
+const navBar = document.querySelector("#sticky_nav");
+const topNavDiv = document.querySelector(".top_nav");
+const namePrintArea = document.querySelector("#namePrint");
+const logOutButton = document.querySelector("#logOut");
 
-window.addEventListener('scroll',()=>{
-        const navOffset = navBar.offsetTop
-        const pageOffset = window.pageYOffset
-        if(pageOffset > navOffset+30){
-                navBar.classList.add('sticky')
-        }else{
-                navBar.classList.remove('sticky')
-        }
-})
+window.addEventListener("scroll", () => {
+  const navOffset = navBar.offsetTop;
+  const pageOffset = window.pageYOffset;
+  if (pageOffset > navOffset + 30) {
+    navBar.classList.add("sticky");
+  } else {
+    navBar.classList.remove("sticky");
+  }
+});
 // const emailValue = emailAddress.value;
 
-
-menuButton.addEventListener("click", () =>{
-        // console.log("hello world")
-        navLinks.classList.toggle("hide")
-})
-
-
-
+menuButton.addEventListener("click", () => {
+  // console.log("hello world")
+  navLinks.classList.toggle("hide");
+});
 
 currentForm.addEventListener("submit", (e) => {
-        // const arr = []
-        e.preventDefault();
-        // console.log("SUBMITTED SUCCESSFULLY");
-        alert("Submitted Successfully")
-        // console.log(emailValue)
-        console.log(emailAddress.value)
-        // emailAddress.value = " "
-        const storage ={
-                email: emailAddress.value,
-        }
-        JSON.stringify(storage)
-        localStorage.setItem("email", storage.email)
-})
+  // const arr = []
+  e.preventDefault();
+  // console.log("SUBMITTED SUCCESSFULLY");
+  alert("Submitted Successfully");
+  // console.log(emailValue)
+  console.log(emailAddress.value);
+  // emailAddress.value = " "
+  const storage = {
+    email: emailAddress.value,
+  };
+  JSON.stringify(storage);
+  localStorage.setItem("email", storage.email);
+});
 // })
 // const logOutButton = document.querySelector("#logOut");
 
@@ -49,26 +45,26 @@ currentForm.addEventListener("submit", (e) => {
 // Rules
 // 1. only logged in user can goto index.html
 // 2. if user is not logged in they should be redirected to login page
-// 2. if user is logged in they cannot visit signup or login page and should be redirected back to index.html 
+// 2. if user is logged in they cannot visit signup or login page and should be redirected back to index.html
 // */
 
 window.onload = () => {
   const getCurrentLoggedUser = localStorage.getItem("logged_user");
-  if(getCurrentLoggedUser){
-        topNavDiv.classList.add("displayNone")
-        const parseUser = JSON.parse(getCurrentLoggedUser)
-        namePrintArea.innerHTML = ` Hi, ${parseUser.firstName + " " + parseUser.lastName} 👋` 
-        // console.log(parseUser.firstName)
-  } else{
-        logOutButton.classList.add("displayNone")
+  if (getCurrentLoggedUser) {
+    topNavDiv.classList.add("displayNone");
+    const parseUser = JSON.parse(getCurrentLoggedUser);
+    namePrintArea.innerHTML = ` Hi, ${
+      parseUser.firstName + " " + parseUser.lastName
+    } 👋`;
+    // console.log(parseUser.firstName)
+  } else {
+    logOutButton.classList.add("displayNone");
+    navLinks.classList.add("marginNavLinks");
   }
+};
 
-}
-
-logOutButton.addEventListener("click", ()=>{
-        localStorage.removeItem("logged_user")
-        window.location.replace('index.html')
-        console.log("button clicked")
-
-})
-
+logOutButton.addEventListener("click", () => {
+  localStorage.removeItem("logged_user");
+  window.location.replace("index.html");
+  console.log("button clicked");
+});
--- signUp.js
+++ signUp.js
@@ -1,77 +1,83 @@
-const inputForm = document.querySelector(".inputForm")
-const firstName = document.querySelector("#firstName")
-const lastName = document.querySelector("#lastName")
-const emailAddress = document.querySelector("#email")
-const pass = document.querySelector("#password")
-const checkBox = document.querySelector("#checkbox_")
-const signUpButton = document.querySelector("#signUpButton")
+const inputForm = document.querySelector(".inputForm");
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const emailAddress = document.querySelector("#email");
+const pass = document.querySelector("#password");
+const checkBox = document.querySelector("#checkbox_");
+const signUpButton = document.querySelector("#signUpButton");
 
-const users = []
+const users = [];
 
-window.onload = ()=>{
-    const getCurrentLoggedUser = localStorage.getItem('logged_user')
+window.onload = () => {
+  const getCurrentLoggedUser = localStorage.getItem("logged_user");
 
-    // if(getCurrentLoggedUser) window.location.replace('index.html')
+  // if(getCurrentLoggedUser) window.location.replace('index.html')
+};
+
+inputForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (
+    !firstName.value &&
+    !lastName.value &&
+    !emailAddress.value &&
+    !pass.value
+  ) {
+    alert("Please Enter all the details");
+    return;
+  }
+
+  if (!checkBox.checked) {
+    alert("Please agree to the checkbox");
+    return;
+  }
+  const user = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: emailAddress.value,
+    password: pass.value,
+  };
+  firstName.value = "";
+  lastName.value = "";
+  emailAddress.value = "";
+  pass.value = "";
+  checkBox.checked = false;
+  storeUser(user);
+  // localStorage.setItem("firstName", firstName.value)
+  window.location.href = "logIn.html";
+});
+
+function storeUser(user) {
+  const previouseUser = retriveUser(user);
+  // console.log(previouseUser)
+  if (previouseUser.found) {
+    alert("User already exists Please Log In");
+    return;
+  }
+  const usersList = getAllUser();
+  // console.log(usersList)
+  usersList.push(user);
+  localStorage.setItem("users", JSON.stringify(usersList));
 }
 
-inputForm.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    if(!firstName.value && !lastName.value && !emailAddress.value && !pass.value){
-        alert("Please Enter all the details")
-        return 
+function retriveUser(user) {
+  const prevUsers = localStorage.getItem("users");
+  if (prevUsers) {
+    const allUsers = JSON.parse(prevUsers);
+    const prevUser = allUsers.find(
+      (currentUser) => currentUser.email === user.email
+    );
+    if (prevUser) {
+      return { found: true, user: prevUser };
+    } else {
+      return { found: false, user: user };
     }
-
-    if(!checkBox.checked){
-        alert("Please agree to the checkbox")
-        return;
-    }
-    const user = {
-        firstName:firstName.value,
-        lastName: lastName.value,
-        email: emailAddress.value,
-        password: pass.value
-    }
-    firstName.value = ""  
-    lastName.value = ""  
-    emailAddress.value = ""  
-    pass.value = ""  
-    checkBox.checked = false  
-    storeUser(user)
-    // localStorage.setItem("firstName", firstName.value)
-    window.location.href="logIn.html"
-})
-
-function storeUser(user){
-    const previouseUser = retriveUser(user)
-    // console.log(previouseUser)
-    if(previouseUser.found){
-        alert("User already exists Please Log In")
-        return
-    }
-    const usersList = getAllUser()
-    // console.log(usersList)
-    usersList.push(user)
-    localStorage.setItem('users',JSON.stringify(usersList))
+  } else {
+    return { found: false, user: user };
+  }
 }
 
-function retriveUser(user){ 
-    const prevUsers = localStorage.getItem('users')
-    if(prevUsers){
-        const allUsers = JSON.parse(prevUsers)
-        const prevUser = allUsers.find(currentUser => currentUser.email === user.email)
-        if(prevUser){
-            return {found:true,user:prevUser}
-        }else{
-            return {found:false,user:user}
-        }
-    }else{
-        return {found:false,user:user}
-    }
+function getAllUser() {
+  const allUser = localStorage.getItem("users");
+  if (allUser) return JSON.parse(allUser);
+  return [];
 }
-
-function getAllUser(){
-    const allUser = localStorage.getItem('users')
-    if(allUser) return JSON.parse(allUser)
-        return []
-}
-
